84121513d698d6dacd02f0f36be6c02d2d4d0baf
km <rudals0731@naver.com>
2019-02-06 15:15:40 +0900

    요청성공 여부 코드 상수 추가

--- wedding/src/main/java/com/co/wedding/bo/BoConstant.java
+++ wedding/src/main/java/com/co/wedding/bo/BoConstant.java
@@ -10,4 +10,8 @@ public final class BoConstant {
 	
 	public static final String BO_USER = "/bo/user";
 	public static final String BO_HALL = "/bo/hall";
+	
+	/* 요청 성공 여부 코드 */
+	public static final String REQ_SUCCESS_CODE = "200";
+	public static final String REQ_FAIL_CODE = "400";
 }
