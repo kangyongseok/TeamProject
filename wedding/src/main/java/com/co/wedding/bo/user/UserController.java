95ac2711888c048a4fcfbc013ce302724186473e
km <rudals0731@naver.com>
2019-02-06 15:22:24 +0900

    등록, 수정 추가

--- wedding/src/main/java/com/co/wedding/bo/user/UserController.java
+++ wedding/src/main/java/com/co/wedding/bo/user/UserController.java
@@ -6,7 +6,9 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.co.wedding.bo.BoConstant;
 import com.co.wedding.info.user.UserBean;
@@ -40,10 +42,51 @@ public class UserController {
 	
 	/**
 	 * 유저 폼
+	 * @param model
+	 * @param bean
 	 */
 	@RequestMapping(value="/NR_form.do")
-    public void form() {}
-	
-	
+    public void form(ModelMap model, UserBean bean) {
+		
+		UserBean dataBean = new UserBean();
+		
+		// 등록
+		if ( bean.getUserId() == null ) {
+			dataBean.setCmd("I");
+		}
+		// 수정
+		else {
+			dataBean.setCmd("U");
+			dataBean = userService.view(bean);
+		}
+		
+		model.addAttribute("dataBean", dataBean);
+	}
+
+	/**
+	 * 유저 등록
+	 * @param bean
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value="/AR_insertAction.do")
+	public String insertAction(UserBean bean) {
+		int affected = userService.insertAction(bean);
+		if (affected == 1) return BoConstant.REQ_SUCCESS_CODE;
+		return BoConstant.REQ_FAIL_CODE;
+	}
+
+	/**
+	 * 유저 수정
+	 * @param bean
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value="/AR_updateAction.do")
+	public String updateAction(UserBean bean) {
+		int affected = userService.updateAction(bean);
+		if (affected == 1) return BoConstant.REQ_SUCCESS_CODE;
+		return BoConstant.REQ_FAIL_CODE;
+	}
 	
 }
