e6920cc9c087924c5baff9a1666295af1862731d
km <rudals0731@naver.com>
2019-02-12 22:57:11 +0900

    hall url 추가

--- wedding/src/main/java/com/co/wedding/fe/FeConstant.java
+++ wedding/src/main/java/com/co/wedding/fe/FeConstant.java
@@ -7,6 +7,8 @@ public final class FeConstant {
      */
 	/* FRONT_END 기본 */
 	public static final String FE_URL = "/fe";
+	/* 웨딩홀 */
+	public static final String FE_HALL_URL = "/fe/hall";
 	/* 임시 test 추후 삭제*/
 	public static final String FE_TEST_URL = "/fe/test";
 }
